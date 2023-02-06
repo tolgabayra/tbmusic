6d5cb6dd77056b0d1fd19822e56aaa283ac3da02
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-02-06 12:31:14 +0300

    Csv usecols parameters

--- python_csv_processing/main.py
+++ python_csv_processing/main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("music.csv")
+col_names = [
+    "title"
+]
+
+df = pd.read_csv("music.csv", header=0, usecols=["title", "artist", "top genre", "year", "dnce"])
+df
+
 
 print(df.to_string())
