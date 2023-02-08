dc21c8c15bf3e46bab9032ab67d6e2b6ee89961b
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-02-08 19:33:05 +0300

    fastapi added

--- python_server/routes/data_routes.py
+++ python_server/routes/data_routes.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Response
 
 app = APIRouter()
 
 
 @app.get("/data_processing")
-def process_data():
+def process_data(request: Request, response: Response):
     return {"message": "Merhaba"}
